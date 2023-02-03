a6326d3ea0e93ed70bc052e4b509c1c5ca6bd3a6
robertrivas <r_rivas@csu.fullerton.edu>
2023-02-02 22:10:03 -0800

    added more structure and files

--- config.py
+++ config.py
@@ -2,20 +2,30 @@
 
 import os
 
+
 class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MANAGE_MAIL_SUBJECT_PREFIX = '[Manage]'
+    MANAGE_MAIL_SENDER = 'Manage Admin <manage@example.com>'
+    MANAGE_ADMIN = os.environ.get('MANAGE_ADMIN')
     BCRYPT_LOG_ROUNDS = 13
     TOKEN_EXPIRATION_DAYS = 30
     TOKEN_EXPIRATION_SECONDS = 0
 
-
     @staticmethod
     def init_app(app):
         pass
+
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
